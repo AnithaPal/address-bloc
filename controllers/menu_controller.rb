a359727b905cbd62d1d1f5d53e93ebd156e4a30c
Anitha <anitha.aravindvel@gmail.com>
2015-06-05 14:52:54 -0400

    all methods added into menu controller

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -30,7 +30,8 @@ class MenuController
       main_menu
     when 3
       system "clear"
-      search_entry
+      #search_entry
+      search_entries
       main_menu
     when 4
       system "clear"
@@ -72,7 +73,10 @@ class MenuController
  
      when "n"
      when "d"
+      delete_entry(entry)
      when "e"
+      edit_entry(entry)
+      entry_submenu(entry)
      when "m"
        system "clear"
        main_menu
@@ -97,8 +101,8 @@ class MenuController
     puts "New entry created"
   end
 
-  def search_entry
-  end
+  # def search_entry
+  # end
 
   def read_csv
     print "Enter CSV file to import: "
@@ -141,6 +145,51 @@ class MenuController
        puts "Updated entry:"
        puts entry
   end
+
+
+  def search_entries
+
+     print "Search by name: "
+     name = gets.chomp
+ 
+     match = @address_book.binary_search(name)
+     system "clear"
+ 
+     if match
+       puts match.to_s
+       search_submenu(match)
+     else
+       puts "No match found for #{name}"
+     end
+   end
+
+   def search_submenu(entry)
+ 
+     puts "\nd - delete entry"
+     puts "e - edit this entry"
+     puts "m - return to main menu"
+     selection = gets.chomp
+
+     case selection
+     when "d"
+       system "clear"
+       delete_entry(entry)
+       main_menu
+     when "e"
+       edit_entry(entry)
+       system "clear"
+       main_menu
+     when "m"
+       system "clear"
+       main_menu
+     else
+       system "clear"
+       puts "#{selection} is not a valid input"
+       puts entry.to_s
+       search_submenu(entry)
+     end
+   end
+
 
 end  
 
