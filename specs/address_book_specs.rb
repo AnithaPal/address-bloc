8360834ff3908cbaf31974d4e32bc05aa8c5e07a
Anitha <anitha.aravindvel@gmail.com>
2015-06-04 21:41:53 -0400

    import_from_csv file test method is updated

--- specs/address_book_specs.rb
+++ specs/address_book_specs.rb
@@ -1,5 +1,15 @@
 RSpec.describe AddressBook do
   let(:book) {AddressBook.new} #This is  asubtitute for book = AddressBook.new
+
+  #helper method to reduce code redundancy. To test csv entries we have 5 different set of entries.Instead of writing 5 times. 
+  #create a method and use it 5 times.
+  def check_entry(entry, expected_name, expected_number, expected_email)
+    expect(entry.name).to eql expected_name
+    expect(entry.phone_number).eo eql expected_number
+    expect(entry.email).to eql expected_email
+  end 
+
+
   context "attributes" do
     it "should respond to entries" do
       #book = AddressBook.new
@@ -52,10 +62,10 @@ RSpec.describe AddressBook do
     it "it checks details of the first entry" do
       book.import_from_csv("entries.csv")
       entry_one = book.entries[0]
-
-      expect(entry_one.name).to eql "Bill"
-      expect(entry_one.phone_number).to eql "555-555-5555"
-      expect(entry_one.email).to eql "bill@blocmail.com"
+      check_entry(entry_one, "Bill", "555-555-5555", "bill@blocmail.com")
+      #expect(entry_one.name).to eql "Bill"
+      #expect(entry_one.phone_number).to eql "555-555-5555"
+      #expect(entry_one.email).to eql "bill@blocmail.com"
 
     end
 
@@ -63,36 +73,40 @@ RSpec.describe AddressBook do
        book.import_from_csv("entries.csv")
        # Check the second entry
        entry_two = book.entries[1]
-       expect(entry_two.name).to eql "Bob"
-       expect(entry_two.phone_number).to eql "555-555-5555"
-       expect(entry_two.email).to eql "bob@blocmail.com"
+       check_entry(entry_two,"Bob", "555-555-5555", "bob@blocmail.com")
+       #expect(entry_two.name).to eql "Bob"
+       #expect(entry_two.phone_number).to eql "555-555-5555"
+       #expect(entry_two.email).to eql "bob@blocmail.com"
      end
  
      it "checks the details of the third entry" do
        book.import_from_csv("entries.csv")
        # Check the third entry
        entry_three = book.entries[2]
-       expect(entry_three.name).to eql "Joe"
-       expect(entry_three.phone_number).to eql "555-555-5555"
-       expect(entry_three.email).to eql "joe@blocmail.com"
+       check_entry(entry_three, "Joe", "555-555-5555", "joe@blocmail.com")
+       #expect(entry_three.name).to eql "Joe"
+       #expect(entry_three.phone_number).to eql "555-555-5555"
+       #expect(entry_three.email).to eql "joe@blocmail.com"
      end
  
      it "checks the details of the fourth entry" do
        book.import_from_csv("entries.csv")
        # Check the fourth entry
        entry_four = book.entries[3]
-       expect(entry_four.name).to eql  "Sally"
-       expect(entry_four.phone_number).to eql "555-555-5555"
-       expect(entry_four.email).to eql "sally@blocmail.com"
+       check_entry(entry_four, "Sally", "555-555-5555", "sally@blocmail.com")
+       #expect(entry_four.name).to eql  "Sally"
+       #expect(entry_four.phone_number).to eql "555-555-5555"
+       #expect(entry_four.email).to eql "sally@blocmail.com"
      end
  
      it "checks the details of the fifth entry" do
        book.import_from_csv("entries.csv")
        # Check the fifth entry
        entry_five = book.entries[4]
-       expect(entry_five.name).to eql "Sussie"
-       expect(entry_five.phone_number).to eql "555-555-5555"
-       expect(entry.email).to eql "sussie@blocmail.com"
+       check_entry(entry_five, "Sussie", "555-555-5555", "sussie@blocmail.com")
+       #expect(entry_five.name).to eql "Sussie"
+       #expect(entry_five.phone_number).to eql "555-555-5555"
+       #expect(entry.email).to eql "sussie@blocmail.com"
      end
   end  
 
